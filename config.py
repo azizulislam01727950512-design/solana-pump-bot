374b7bc08e10e027ef3f122c7b65883eb0f8f6a3
azizulislam01727950512-design <azizulislam01727950512@gmail.com>
2026-08-21 09:57:14 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 # config.py - বটের সব সেটিংস ও ডেটাবেজ ফাইল
 
-BOT_TOKEN = "8862192124:AAHBuMhTdnd50mTByNMsiTMZs-tEWpTEGfs"
-bot = BOT_TOKEN
-OWNER_WALLET = "8efY9t9MCLKGPSKa5qahN9ogzbro5q87i3XgENLGfwCr" 
+import telebot
+
+BOT_TOKEN = "8862192124:AAHBuWhTdndS0mTBvNWsiTMZs-tEWpTEGfs"
+bot = telebot.TeleBot(BOT_TOKEN)
+OWNER_WALLET = "BeFY9t9MCLKGP5Ka5qahN9ogzbro5q87iJXgEMLGfWCr"
 
 user_database = {}
 active_messages = {}
 
-# পাম্প ডট ফানের লাইভ টোকেন ডেটা (আপনার স্ক্রিনশটের অবিকল রূপ)
+# পাম্প ডট ফানের লাইভ টোকেন ডেটা
 live_tokens = {
-    "token_1": {"name": "$Coal", "price": "$0.00404", "mcap": 7200, "tax": "89\n56/33", "status": "🛡️ SAFE", "address": "SOL...abc123"},
-    "token_2": {"name": "$NGL", "price": "$0.00077", "mcap": 13200, "tax": "37\n30/7", "status": "🛡️ SAFE", "address": "SOL...xyz456"},
-    "token_3": {"name": "$LUCY", "price": "$0.00174", "mcap": 6200, "tax": "94\n80/14", "status": "🚨 RUG RISK", "address": "SOL...lmn789"}
+    "token_1": {"name": "$Goal", "price": "$0.00404", "mcap": 7200, "tax": "891nS6/33", "status": "🛡️ SAFE", "address": ""},
+    "token_2": {"name": "$NGL", "price": "$0.00077", "mcap": 13200, "tax": "371n30/7", "status": "🛡️ SAFE", "address": ""},
+    "token_3": {"name": "$LUCY", "price": "$0.00174", "mcap": 6200, "tax": "941n80/14", "status": "🚨 RUG RISK", "address": ""}
 }
